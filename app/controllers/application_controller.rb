db1bb3f891cb97385be51a38d7c00dbe7be96f75
shouechi <hajimete.1105@gmail.com>
2025-01-08 22:07:36 +0900

    ログアウト機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,9 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     posts_path
   end
+
+  # ログアウト後ログイン画面へ遷移
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
 end
