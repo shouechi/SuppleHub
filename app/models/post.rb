4fb43bf1880a8177257d82223c654da326a1267c
shouechi <hajimete.1105@gmail.com>
2025-01-11 22:53:26 +0900

    投稿一覧作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
+  belongs_to :user
+  belongs_to :supplecategory
+
+  validates :effect, presence: true
+  validates :side_effect, presence: true
+  validates :supple_image, presence: true
 end
