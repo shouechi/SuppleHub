422a9f9a7a60ee48f04608ed0bc3fa55bc4f7b92
shouechi <hajimete.1105@gmail.com>
2025-02-18 00:04:44 +0900

    ログイン、新規登録修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :recoverable
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, uniqueness: true
