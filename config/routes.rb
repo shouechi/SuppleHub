df006149813352a69608b3512ced27a42912e13d
shouechi <hajimete.1105@gmail.com>
2025-02-24 22:58:26 +0900

    ルーテイングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,12 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  resources :posts
+  resources :posts do
+    collection do
+      get :autocomplete
+    end
+  end
+  resources :supplecategories
 
   resources :users, only: %i[ show ]
 
