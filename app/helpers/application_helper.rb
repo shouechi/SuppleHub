b31c548303a608c3f9a1ef016d9c21238fc98a90
shouechi <hajimete.1105@gmail.com>
2025-03-27 22:06:38 +0900

    OGP実装

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,31 @@ module ApplicationHelper
     else "bg-gray-500"
     end
   end
+
+  def default_meta_tags
+    {
+      site: "SuppleHub",
+      title: "サプリメントを共有できるサービス",
+      reverse: true,
+      charset: "utf-8",
+      description: "supplehubはサプリメントを共有できるサービスです。サプリメントの効果だけなく副作用も知ることができます。",
+      keywords: "サプリメント,効果,副作用",
+      canonical: "https://supplehub.onrender.com",
+      separator: "|",
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: "website",
+        url: "https://supplehub.onrender.com/",
+        image: image_url("ogp.png"),
+        local: "ja_JP"
+      },
+      twitter: {
+        card: 'summary_large_image', # Twitterで表示する場合は大きいカードに変更
+        site: '@supplehub',
+        image: image_url('ogp.png')
+      }
+    }
+  end
 end
